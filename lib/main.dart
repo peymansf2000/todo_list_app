2d019935bc3190cd143da98b129800015a361b5e
peymansf2000 <peymansf2000@outlook.com>
2023-08-30 20:02:31 +0330

    Update Theme data

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'objectbox.dart';
 import 'homeScreen.dart';
+
 /// Provides access to the ObjectBox Store throughout the app.
 late ObjectBox objectbox;
 
 Future<void> main() async {
-  // This is required so ObjectBox can get the application directory
-  // to store the database in.
+  SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(statusBarColor: primaryVariantColor));
   WidgetsFlutterBinding.ensureInitialized();
 
   objectbox = await ObjectBox.create();
 
-  runApp(MyApp());
+  runApp(const MyApp());
 }
+
 const primaryTextColor = Color(0xff1D2830);
 const Color primaryColor = Color(0xff794CFF);
 const Color primaryVariantColor = Color(0xff5C0AFF);
@@ -21,7 +24,6 @@ const secondaryTextColor = Color(0xffAFBED0);
 const normalPriority = Color(0xffF09819);
 const lowPriority = Color(0xff3BE1F1);
 const highPriority = primaryColor;
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -32,10 +34,12 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'ToDO',
       theme: ThemeData(
-        textTheme: GoogleFonts.poppinsTextTheme(const TextTheme(
-              headline6: TextStyle(fontWeight: FontWeight.bold))),
-        useMaterial3: true,
-        colorScheme: const ColorScheme.light(
+          textTheme: GoogleFonts.poppinsTextTheme(const TextTheme(titleSmall: TextStyle(fontWeight: FontWeight.bold),
+              titleLarge:
+                  TextStyle(fontWeight: FontWeight.bold, color: Colors.white))),
+                  
+          useMaterial3: true,
+          colorScheme: const ColorScheme.light(
               primary: primaryColor,
               primaryContainer: primaryVariantColor,
               background: Color(0xffF3F5F8),
@@ -43,11 +47,8 @@ class MyApp extends StatelessWidget {
               onPrimary: Colors.white,
               onBackground: primaryTextColor,
               secondary: primaryColor,
-              onSecondary: Colors.white)
-      ),
+              onSecondary: Colors.white)),
       home: const HomeScreen(),
     );
   }
 }
-
-
